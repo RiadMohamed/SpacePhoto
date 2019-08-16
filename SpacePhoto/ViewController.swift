03c25691da787b398a28e9ce8bced8af04da15e0
Riad Mohamed <riaduk1@gmail.com>
2019-08-17 00:19:14 +0200

    Added Extension to the URL data type to allow appending URLs with queries
    Added PhotoInfoController to send a request to the NASA API for the APOD with completion handler.
    
    Defined the data structure that will hold the JSON data coming from the NASA API.
    
    Added view components to display the data to the user and embedded in stack and nav controller.

--- SpacePhoto/ViewController.swift
+++ SpacePhoto/ViewController.swift
@@ -1,18 +1,30 @@
-//
-//  ViewController.swift
-//  SpacePhoto
-//
-//  Created by Riad Mohamed on 8/16/19.
-//  Copyright © 2019 Riad Mohamed. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var copyrightLabel: UILabel!
+    
+    let photoInfoController = PhotoInfoController()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        descriptionLabel.text = ""
+        copyrightLabel.text = ""
+        photoInfoController.fetchPhotoInfo { (photoInfo) in
+            if let photoInfo = photoInfo {
+                DispatchQueue.main.async {
+                    self.title = photoInfo.title
+                    self.descriptionLabel.text = photoInfo.description
+                    if let copyright = photoInfo.copyright {
+                        self.copyrightLabel.text = "Copyright \(copyright)"
+                    } else {
+                        self.copyrightLabel.isHidden = true
+                    }
+                }
+            }
+        }
     }
 
 
